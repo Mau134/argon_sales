073fbbfc3f8a03ab98e1d130e8ce74a7d2a42972
Maurice <molande.mau@gmail.com>
2025-07-28 20:31:20 +0200

    Fixed Database

--- argonsales.sql
+++ argonsales.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 28, 2025 at 07:19 PM
--- Server version: 10.4.32-Mariadb
+-- Generation Time: Jul 28, 2025 at 08:20 PM
+-- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -35,7 +35,7 @@ CREATE TABLE `deleted_products` (
 -- --------------------------------------------------------
 
 --
--- Table strudbure for table `inventory`
+-- Table structure for table `inventory`
 --
 
 CREATE TABLE `inventory` (
@@ -48,22 +48,13 @@ CREATE TABLE `inventory` (
   `ordering_price` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---db
--- Dumping data for table `inventory`
---
-
-INSERT INTO `inventory` (`id`, `product_name`, `category`, `quantity`, `updated_at`, `selling_price`, `ordering_price`) VALUES
-(8, 'Mens blue suit size 23', 'Suits', 23, '2025-07-28 10:57:08', 250000.00, 180000.00),
-(15, 'Gucci Handbag', 'Bag', 59, '2025-07-28 16:16:35', 55000.00, 34000.00),
-(17, 'Golf Shirt', 'Clothes', 0, '2025-07-28 16:40:55', 60000.00, 20000.00);
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `records`
 --
 
-CREATE TABLE dbecords` (
+CREATE TABLE `records` (
   `id` int(11) NOT NULL,
   `action` varchar(100) DEFAULT NULL,
   `details` text DEFAULT NULL,
@@ -79,7 +70,7 @@ CREATE TABLE dbecords` (
 
 CREATE TABLE `sales` (
   `id` int(11) NOT NULL,
-  `product_iddbint(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
   `product_name` varchar(255) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `sale_date` timestamp NOT NULL DEFAULT current_timestamp(),
@@ -90,17 +81,6 @@ CREATE TABLE `sales` (
   `ordering_price` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `sales`
---
-
-INSERT INTO `sales` (`id`, `product_id`, `product_name`, `user_id`, `sale_date`, `total_amount`, `created_at`, `quantity_sold`, `selling_price`, `ordering_price`) VALUES
-(38, 8, 'Mens blue suit size 23', NULL, '2025-07-28 10:57:08', 250000.00, '2025-07-28 10:57:08', 1, 250000.00, 180000.00),
-(39, 15, 'Gucci Handbag', NULL, '2025-07-28 16:16:35', 165000.00, '2025-07-28 16:16:35', 3, 55000.00, 34000.00),
-(40, 17, NULL, NULL, '2025-07-28 16:40:15', 240000.00, '2025-07-28 16:40:15', 4, 60000.00, 20000.00),
-(41, 17, NULL, NULL, '2025-07-28 16:40:37', 720000.00, '2025-07-28 16:40:37', 12, 60000.00, 20000.00),
-(42, 17, NULL, NULL, '2025-07-28 16:40:55', 240000.00, '2025-07-28 16:40:55', 4, 60000.00, 20000.00);
-
 -- --------------------------------------------------------
 
 --
@@ -109,7 +89,7 @@ INSERT INTO `sales` (`id`, `product_id`, `product_name`, `user_id`, `sale_date`,
 
 CREATE TABLE `sales_items` (
   `id` int(11) NOT NULL,
-  `sale_id` idb(11) DEFAULT NULL,
+  `sale_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
   `unit_price` decimal(10,2) DEFAULT NULL
@@ -124,19 +104,11 @@ CREATE TABLE `sales_items` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `role` enumdbboss','shopkeeper') NOT NULL,
+  `role` enum('boss','shopkeeper') NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `password_hash` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `role`, `email`, `password_hash`, `created_at`) VALUES
-(3, 'Maurice Molande', 'boss', 'molande.mau@gmail.com', '$2y$10$0cj.gAfbM5IEdxpG0TpgE.YlWpUK/hkH/iu/vTsJmZo0Q38AFM03W', '2025-07-20 05:48:24'),
-(4, 'Nohata Molande', 'shopkeeper', 'nohatamolande@gmail.com', '$2y$10$nSuksHzr4we1upIas9OBmeGN27iqryfKLn/6/r2q749hBWVwxHMFm', '2025-07-20 05:58:32');
 
 --
 -- Indexes for dumped tables
